06d9a8ca19bda612e9fa4f3a792dc38d1d6e2db0
Eduarda Almeida <duda.s.almeida10@gmail.com>
2021-12-27 14:34:09 -0300

    fim aula 19

--- Módulo 1 - Python Básico (Lógica de Programação)/Aula 19 - While e Strings/aula19_while-string.py
+++ Módulo 1 - Python Básico (Lógica de Programação)/Aula 19 - While e Strings/aula19_while-string.py
@@ -1,11 +1,18 @@
 frase = 'o rato roeu a roupa do rei de roma'
 tamanho = len(frase)
 new_string = ''
+letra = ''
 c = 0
 
+input_user = input('Qual letra deseja colocar maiuscula: ')
+
 while c < tamanho:
-    #print(frase[c])
-    new_string += frase[c]
+    ## print(frase[c])
+    letra = frase[c]
+    if letra == input_user:
+        new_string += input_user.upper()
+    else:    
+        new_string += letra
     c += 1
 
 print(new_string)
